bb736ad8bbd671e80b45597fa88a5b05639c91d9
m4h7 <m4h7@users.noreply.github.com>
2016-12-13 20:37:03 +0000

    Added ocaml travis config.

---
+++ .travis-ci.sh
@@ -0,0 +1,26 @@
+# credit: http://anil.recoil.org/2013/09/30/travis-and-ocaml.html
+# Edit this for your own project dependencies
+OPAM_DEPENDS="ocamlfind ounit re"
+
+case "$OCAML_VERSION,$OPAM_VERSION" in
+3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
+3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
+4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
+4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
+4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
+4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
+*) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
+esac
+
+echo "yes" | sudo add-apt-repository ppa:$ppa
+sudo apt-get update -qq
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+export OPAMYES=1
+opam init
+opam install ${OPAM_DEPENDS}
+eval `opam config env`
+./to_x86
+make native-code
+make byte-code
+# raytracer
+cd min-rt/ ; make
