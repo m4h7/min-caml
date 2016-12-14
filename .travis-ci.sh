5b9bcd368913951a8cbc020a831984583d1089e8
m4h7 <m4h7@users.noreply.github.com>
2016-12-14 09:35:35 +0000

    Add gcc-multilib for i686 compilation.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -14,7 +14,7 @@ esac
 
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
-sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam gcc-multilib
 export OPAMYES=1
 opam init
 opam install ${OPAM_DEPENDS}
